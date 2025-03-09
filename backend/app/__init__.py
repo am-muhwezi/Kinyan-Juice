105322409b1bae58b33cc76f7449aa4f75f1f94c
Yahweh's Delight <michealphenh10@gmail.com>
2025-03-09 10:53:38 +0300

    add: database models setup

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from .utils import db
-from .config.config import config_dict
+from .config.config import Config_dict
 from .auth.views import auth_namespace
 from .models.users_db_model import User
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 
-def create_app(config=config.Config_dict['prod']):
+def create_app(config=Config_dict['prod']):
     app = Flask(__name__)
 
     app.config.from_object(config)
     db.init_app(app)
-    api = Api(app)
+    api = Api(app, title='KinyanJuice', version='1.0', description='KinyanJuice API')
 
     jwt = JWTManager(app)
 
